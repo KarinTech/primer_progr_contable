ed767e72062c1db7aa58db8ff4ee7d9105bbe958
Karin Centenaro <98807488+KarinTech@users.noreply.github.com>
2022-12-20 16:31:25 -0300

    Update balance.py

--- balance.py
+++ balance.py
@@ -1,12 +1,2 @@
-activos=input(input("ingrese ACTIVOS"))
-pasivos=input(input("ingrese PASIVOS"))
 
-def balance(activos, pasivos):
-      if activos>pasivos:
-        return balance 
-  print ("El balance es positivo" )
-      else:
-      print("El balance es negativo")
-      
- balance(activos, pasivos)
 
