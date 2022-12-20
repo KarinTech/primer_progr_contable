bd6fb86ef782a5ded2754d76838a469736f4223c
Karin Centenaro <98807488+KarinTech@users.noreply.github.com>
2022-12-20 14:38:31 -0300

    fixed and added inputs for the accounts values

--- balance.py
+++ balance.py
@@ -1,28 +1,12 @@
-mercadoria = input("Enter the amount of "Mercadoria" )
-                   print("Mercadoria =  ", mercadoria)
-pasivos ={
-"mercadoria",
-"ICMS a Recuperar",
-"fornecedor"
+activos=input(input("ingrese ACTIVOS"))
+pasivos=input(input("ingrese PASIVOS"))
 
-}
-suma_pas=int(mercadoria+18000+35000)
+def balance(activos, pasivos):
+      if activos>pasivos:
+        return balance 
+  print ("El balance es positivo" )
+      else:
+      print("El balance es negativo")
+      
+ balance(activos, pasivos)
 
-activos = {
-    "caixa":50000,
-    "ventas":10000,
-    "flujo_capital":100000
-}
-suma_act=(50000+100000+10000)
-for pasivo in pasivos:
-    print(pasivo, pasivos[pasivo], sep="=")
-   
-
-for activo in activos:
-    print(activo, activos[activo], sep="="), 
-
-
-print("Total Activos = ",suma_act)
-print("Total Pasivos = ",suma_pas)
-balance_simple= (suma_act- suma_pas)   
-print("El balance es de ", balance_simple )
